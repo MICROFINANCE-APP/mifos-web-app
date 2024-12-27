4be8aa3f1c619786c467e9f15a753408865c7e9a
fescii <isfescii@gmail.com>
2024-12-27 15:35:15 +0300

    Update application branding and version information

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241204',
-    'hash': '29cda98b'
+    'version': '241217',
+    'hash': '69ef977d'
   },
   'allow_switching_backend_instance': true
 };
